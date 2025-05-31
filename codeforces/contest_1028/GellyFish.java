7226971fa7c6277cdf6d5513b944518ce39441fe
AbhiramPrabhanjan <abhiramprabhanjan.ragula@gmail.com>
2025-05-31 23:57:37 +0530

    Add sol for cf 1st Prblm

---
+++ codeforces/contest_1028/GellyFish.java
@@ -0,0 +1,31 @@
+// package UPSOLVING.codeforces.contest_1028;
+
+import java.util.*;
+ public class GellyFish
+{
+    public static void main(String[] args) 
+    {
+        // System.out.println("enter tc");
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            int d=sc.nextInt();
+
+            
+
+            if(Math.min(b,d)<=Math.min(a,c))
+            System.out.println("Gellyfish");
+            else
+            System.out.println("Flower");
+
+
+        }
+        
+    }
+
+    
+}s
